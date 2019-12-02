94a7872fce196c97a861447b1682a3204254ca66
Dacio Romero <dacioromero@gmail.com>
2019-12-02 13:51:35 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Web Monetization Types
+TypeScript types for [Web Monetization](https://webmonetization.org).
+
+Similar types will hopefully be added to lib.dom.d.ts if and when Web Monetization catches on.
+
+## Install
+    $ npm install wm-types --save-dev
+
+or
+
+    $ yarn add wm-types --dev
+
+## Usage
+Import for monetization to be added to `document`.
+
+```typescript
+import 'wm-types'
+```
+
+or
+
+```typescript
+import { MonetizationState } from 'wm-types'
+```
---
+++ index.d.ts
@@ -0,0 +1,61 @@
+interface TEventListener<T extends Event = Event> {
+  (evt: T): void
+}
+
+interface TEventListenerObject<T extends Event = Event> {
+  handleEvent(evt: T): void
+}
+
+type TEventListenerOrListenerObject<T extends Event = Event> =
+  | TEventListener<T>
+  | TEventListenerObject<T>
+
+export interface MonetizationEventDetail {
+  paymentPointer: string
+  requestId: string
+}
+
+export interface MonetizationProgressEventDetail extends MonetizationEventDetail {
+  amount: string
+  assetCode: string
+  assetScale: number
+}
+
+export type MonetizationEvent = CustomEvent<MonetizationEventDetail>
+
+export type MonetizationPendingEvent = MonetizationEvent
+export type MonetizationStartEvent = MonetizationEvent
+export type MonetizationStopEvent = MonetizationEvent
+export type MonetizationProgressEvent = CustomEvent<MonetizationProgressEventDetail>
+
+interface MonetizationEventMap {
+  monetizationpending: MonetizationPendingEvent
+  monetizationstart: MonetizationStartEvent
+  monetizationstop: MonetizationStopEvent
+  monetizationprogress: MonetizationProgressEvent
+}
+
+export type MonetizationState = 'stopped' | 'pending' | 'started'
+
+// Most implementations use a div rather instead of a plain EventTarget
+export interface Monetization extends EventTarget {
+  state: MonetizationState
+
+  addEventListener<T extends keyof MonetizationEventMap>(
+    type: T,
+    listener: TEventListenerOrListenerObject<MonetizationEventMap[T]> | null,
+    options?: boolean | AddEventListenerOptions
+  ): void
+
+  removeEventListener<T extends keyof MonetizationEventMap>(
+    type: T,
+    listener: TEventListenerOrListenerObject<MonetizationEventMap[T]> | null,
+    options?: EventListenerOptions | boolean
+  ): void
+}
+
+declare global {
+  interface Document {
+    monetization?: Monetization
+  }
+}
