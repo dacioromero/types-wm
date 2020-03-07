44757480c05315767d9b23af8ffc6a5ec537ea60
Dacio Romero <dacioromero@gmail.com>
2020-03-07 05:33:14 -0800

    Remove MonetizationEventDetail export, add type property to events for type guarding

--- index.d.ts
+++ index.d.ts
@@ -1,42 +1,28 @@
-interface IEventListener<T extends Event = Event> {
-  (evt: T): void
-}
-
-interface IEventListenerObject<T extends Event = Event> {
-  handleEvent(evt: T): void
-}
-
-type IEventListenerOrListenerObject<T extends Event = Event> =
-  | IEventListener<T>
-  | IEventListenerObject<T>
-
-export interface MonetizationEventDetail {
+interface BaseMonetizationEventDetail {
   paymentPointer: string
   requestId: string
 }
 
-export interface MonetizationProgressEventDetail extends MonetizationEventDetail {
-  finalized: boolean
-}
-
-export interface MonetizationProgressEventDetail extends MonetizationEventDetail {
-  amount: string
-  assetCode: string
-  assetScale: number
-}
-
-type GenericMonetizationEvent = CustomEvent<MonetizationEventDetail>
-
-export interface MonetizationPendingEvent extends GenericMonetizationEvent {
+export interface MonetizationPendingEvent extends CustomEvent<BaseMonetizationEventDetail> {
   type: 'monetizationpending'
 }
 
-export interface MonetizationStartEvent extends GenericMonetizationEvent {
+export interface MonetizationStartEvent extends CustomEvent<BaseMonetizationEventDetail> {
   type: 'monetizationstart'
 }
 
-export interface MonetizationStopEvent extends CustomEvent<MonetizationProgressEventDetail> {
+interface MonetizationStopEventDetail extends BaseMonetizationEventDetail {
+  finalized: boolean
+}
+
+export interface MonetizationStopEvent extends CustomEvent<MonetizationStopEventDetail> {
   type: 'monetizationstop'
+}
+
+interface MonetizationProgressEventDetail extends BaseMonetizationEventDetail {
+  amount: string
+  assetCode: string
+  assetScale: number
 }
 
 export interface MonetizationProgressEvent extends CustomEvent<MonetizationProgressEventDetail> {
@@ -54,20 +40,30 @@ export type MonetizationEvent = MonetizationEventMap[keyof MonetizationEventMap]
 
 export type MonetizationState = 'stopped' | 'pending' | 'started'
 
-// Most implementations use a div rather instead of a plain EventTarget
+type EventListener<T, E extends Event = Event> = (this: T, evt: E) => any
+
+interface EventListenerObject<T, E extends Event = Event> {
+  handleEvent(this: T, evt: E): void
+}
+
+type EventListenerOrListenerObject<T, E extends Event = Event> =
+  | EventListener<T, E>
+  | EventListenerObject<T, E>
+
+// Note: The Coil extension uses a <div> instead of an EventTarget
 export interface Monetization extends EventTarget {
   state: MonetizationState
 
-  addEventListener<T extends keyof MonetizationEventMap>(
-    type: T,
-    listener: IEventListenerOrListenerObject<MonetizationEventMap[T]> | null,
+  addEventListener<K extends keyof MonetizationEventMap>(
+    type: K,
+    listener: EventListenerOrListenerObject<Monetization, MonetizationEventMap[K]> | null,
     options?: boolean | AddEventListenerOptions
   ): void
 
-  removeEventListener<T extends keyof MonetizationEventMap>(
-    type: T,
-    listener: IEventListenerOrListenerObject<MonetizationEventMap[T]> | null,
-    options?: EventListenerOptions | boolean
+  removeEventListener<K extends keyof MonetizationEventMap>(
+    type: K,
+    listener: EventListenerOrListenerObject<Monetization, MonetizationEventMap[K]> | null,
+    options?: boolean | EventListenerOptions
   ): void
 }
 
